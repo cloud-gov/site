6d0b2cac7e141dc038f0d6e746286d1249b5e55d
Brian Hurst <brian.hurst@gsa.gov>
2025-02-24 13:51:16 -0500

    Fix config

--- .eleventy.js
+++ .eleventy.js
@@ -442,10 +442,23 @@ module.exports = function (config) {
     // See https://www.11ty.dev/docs/languages/ for other engines.
     htmlTemplateEngine: "liquid",
 
+    // -----------------------------------------------------------------
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Don’t worry about leading and trailing slashes, we normalize these.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+
+    // Optional (default is shown)
+    pathPrefix,
+    // -----------------------------------------------------------------
+
     // These are all optional (defaults are shown):
     dir: {
       input: "content",
-      pathPrefix,
       includes: "../_includes",
       data: "../_data",
       output: "_site",
