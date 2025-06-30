64354263b8e863cd0e4e197848aece3e93e535d5
Sarah Rudder <sarah.rudder@gsa.gov>
2025-06-30 15:16:21 -0500

    add footer/identifier

---
+++ src/data/footer.ts
@@ -0,0 +1,77 @@
+export const footerProps = {
+  list: [
+    {
+      label: "Get in touch",
+      links: [
+        {
+          text: "Customer support",
+          url: "/support"
+        },
+        {
+          text: "Business Inquiries",
+          url: "mailto:inquiries@cloud.gov"
+        }
+      ]
+    },
+    {
+      label: "For developers",
+      links: [
+        {
+          text: "Our repositiories",
+          url: "https://github.com/cloud-gov/"
+        },
+        {
+          text: "Free sandbox",
+          url: "/sign-up"
+        },
+        {
+          text: "Status",
+          url: "https://cloudgov.statuspage.io/"
+        }
+      ]
+    },
+    {
+      label: "Policies",
+      links: [
+        {
+          text: "Vulnerability disclosure policy",
+          url: "https://www.gsa.gov/vulnerability-disclosure-policy"
+        },
+        {
+          text: "Open source policy",
+          url: "https://open.gsa.gov/oss/"
+        }
+      ]
+    }
+  ],
+  identifier: [
+    {
+      text: "About GSA",
+      url: "https://www.gsa.gov/about-us"
+    },
+    {
+      text: "Accessibility statement",
+      url: "https://www.gsa.gov/website-information/accessibility-aids"
+    },
+    {
+      text: "FOIA requests",
+      url: "https://www.gsa.gov/reference/freedom-of-information-act-foia"
+    },
+    {
+      text: "No FEAR Act data",
+      url: "https://www.gsa.gov/reference/civil-rights-programs/notification-and-federal-employee-antidiscrimination-and-retaliation-act-of-2002"
+    },
+    {
+      text: "Office of the Inspector General",
+      url: "https://www.gsaig.gov/"
+    },
+    {
+      text: "Performance reports",
+      url: "https://www.gsa.gov/reference/reports/budget-performance"
+    },
+    {
+      text: "Privacy policy",
+      url: "https://www.gsa.gov/website-information/website-policies"
+    },
+  ]
+};
