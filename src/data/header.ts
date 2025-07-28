a2bdd821f25ee21af678f725c485d497be38a2e2
Cal Callaghan <cal.callaghan@gsa.gov>
2025-07-28 12:58:05 -0700

    Update header to include product brand names. Update product sites to include product branding placeholders. Attempt to add workshop-specific site copy. Thwarted by yaml.

--- src/data/header.ts
+++ src/data/header.ts
@@ -1,28 +1,28 @@
 export const headerProps = {
-  logoText: 'Cloud.gov',
-  logoUrl: '',
+  logoText: "Cloud.gov",
+  logoUrl: "",
   dropdowns: [
     {
-      label: 'Solutions',
+      label: "Solutions",
       links: [
-        { label: 'Fast & Scalable Federal Apps', url: 'apps' },
-        { label: 'Federal Public Websites', url: 'pages' },
-        { label: 'Developer Collaboration Tools', url: 'workshop' }
-      ]
+        { label: "Platform: Fast & Scalable Federal Apps", url: "apps" },
+        { label: "Pages: Federal Public Websites", url: "pages" },
+        { label: "Workshop: Developer Collaboration Tools", url: "workshop" },
+      ],
     },
   ],
   navLinks: [
-    { label: 'Pricing', url: 'pricing' },
-    { label: 'Security & Compliance', url: 'security' },
+    { label: "Pricing", url: "pricing" },
+    { label: "Security & Compliance", url: "security" },
   ],
   secondaryLinks: [
-    { label: 'Support', url: 'support' },
-    { label: 'Contact', url: 'contact' },
-    { label: 'Status', url: 'https://cloudgov.statuspage.io/' }
+    { label: "Support", url: "support" },
+    { label: "Contact", url: "contact" },
+    { label: "Status", url: "https://cloudgov.statuspage.io/" },
   ],
   buttonLinks: [
-    { label: 'Get a free sandbox', url: 'sign-up' },
-    { label: 'Log in', url: 'https://login.fr.cloud.gov', class: 'outline' }
+    { label: "Get a free sandbox", url: "sign-up" },
+    { label: "Log in", url: "https://login.fr.cloud.gov", class: "outline" },
   ],
-  searchPlaceholder: "Search for..."
+  searchPlaceholder: "Search for...",
 };
